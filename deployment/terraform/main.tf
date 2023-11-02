b4c2d809691f7b0355d3436b6a0feec64c421098
ge85fot <arda.hueseyinoglu@tum.de>
2023-11-02 20:11:00 +0100

    ch

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -2,35 +2,11 @@ provider "azurerm" {
   features {}
 }
 
-
-
 resource "azurerm_resource_group" "testing_resource_group" {
   name     = "testing-rg"
   location = "East US"
 }
 
-resource "azurerm_storage_account" "testing_storage_account_1651253" {
-  name                     = "testing-sa"
-  resource_group_name      = azurerm_resource_group.testing_resource_group.name
-  location                 = azurerm_resource_group.testing_resource_group.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "testing_storage_container" {
-  name                  = "terraform-state"
-  storage_account_name  = azurerm_storage_account.testing_storage_account_1651253.name
-  container_access_type = "private"
-}
-
-terraform {
-  backend "azurerm" {
-    resource_group_name   = azurerm_resource_group.testing_resource_group.name
-    storage_account_name  = azurerm_storage_account.testing_storage_account_1651253.name
-    container_name        = azurerm_storage_container.testing_storage_container.name
-    key                   = "terraform.tfstate"
-  }
-}
 
 resource "azurerm_virtual_network" "testing_resource" {
   name                = "testing-network"
