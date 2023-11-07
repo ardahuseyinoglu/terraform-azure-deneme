cbf07aefc1394c80a9cf49839f860bce067e9e0f
ge85fot <arda.hueseyinoglu@tum.de>
2023-11-07 15:33:44 +0100

    aa

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -2,3 +2,5 @@ export REGION=westeurope
 export RG_NAME=rg-tfstate-deneme
 export STORAGE_ACCOUNT_NAME=satfstatedeneme13827
 export STORAGE_CONTAINER_NAME=sc-tfstate
+export SP_NAME=tf-state-storage-sp
+export SP_ROLE=Contributor
