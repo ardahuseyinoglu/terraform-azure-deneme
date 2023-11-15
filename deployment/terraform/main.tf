7268415d2fad8676c4c2ccd7984f467054e4b8f9
ge85fot <arda.hueseyinoglu@tum.de>
2023-11-15 22:16:26 +0100

    aa34

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name = "test_azure_openai_resource_group1"
-    storage_account_name = "test_azure_openai_tfstate_storage8374bsvq32"
+    storage_account_name = "testazureopenaitfstate451685"
     container_name = "test_azure_openai_tfstate_container"
     key = "terraform.tfstate"
   }
